db98b10f1552ec7b5023a50f9f38cc9d5e18e513
Ashot Martirosyan <mashot2@gmail.com>
2019-07-17 21:40:04 +0400

    Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
 $(document).ready(function () {
+	if ($(document).scrollTop() > 1) {
+		$('.my-navbar').removeClass('hide');
+		$('.my-number').addClass('hide');
+		$('.my-btn').addClass('btn-outline-primary');
+		$('.my-btn').removeClass('border-white');
+		$('.my-btn').removeClass('text-white');
+		$('.my-nav').removeClass('bg-transparent');
+		$('.my-nav').addClass('bg-white');
+		$('.my-nav').removeClass('navbar-in-top');
+	} else {
+		$('.my-number').removeClass('hide');
+		$('.my-navbar').addClass('hide');
+		$('.my-btn').removeClass('btn-outline-primary');
+		$('.my-btn').addClass('border-white');
+		$('.my-btn').addClass('text-white');
+		$('.my-nav').addClass('bg-transparent');
+		$('.my-nav').removeClass('bg-white');
+		$('.my-nav').addClass('navbar-in-top');
+	}
 	$(document).scroll(function () {
 		if ($(document).scrollTop() > 1) {
 			$('.my-navbar').removeClass('hide');
